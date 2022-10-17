e4b7b2a7a25d22df0d9cbd18763d8e5ba6aa43ca
Ahad Aman <ahadaman@jataq.com>
2022-10-17 16:40:59 +0500

    nav

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+const Navbar = () => {
+  return (
+    <>
+      <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav"></ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+export default Navbar;
--- src/Pages/Homepage/Home.js
+++ src/Pages/Homepage/Home.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
 const Home = () => {
-    return (
-        <>
-            Home
-        </>
-    );
-}
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
 
-export default Home 
+export default Home;
