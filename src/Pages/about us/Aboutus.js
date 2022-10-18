81256b309fc4faa3e85cf6da297a1a541f24bb89
Kashif Ali Khan <kashifalykhan1999@gmail.com>
2022-10-18 20:37:23 +0500

    aboutus com

--- src/Pages/about us/Aboutus.js
+++ src/Pages/about us/Aboutus.js
@@ -1,5 +1,9 @@
 import Navbar from "../../Components/Navbar/Navbar";
 import aboutimg from "../../Assets/Images/Rectangle 1100.png";
+import teammam from "../../Assets/Images/Ellipse 1200.png";
+import teammam2 from "../../Assets/Images/Ellipse 1201.png";
+import teammam3 from "../../Assets/Images/Ellipse 1202.png";
+import teammam4 from "../../Assets/Images/Ellipse 1203.png";
 import "./aboutus.css";
 const AboutUs = () => {
   return (
@@ -7,8 +11,8 @@ const AboutUs = () => {
       <Navbar />
       <div className="aboutusmain">{/* <img src={aboutimg} /> */}</div>
       <div className="container">
-        <div row>
-          <h1 className="px-5 my-5">ABOUT US</h1>
+        <div className="row justify-content-center">
+          <h1 className="px-5 col-md-11 my-5">ABOUT US</h1>
           <p className="ogsfonts18 col-md-11 my-5 aboutusp2 px-5">
             OGS MANPOWER is an authorized international Recruitment, employment
             organization having headquartered in Rawalpindi-Islamabad Pakistan.
@@ -23,7 +27,7 @@ const AboutUs = () => {
             Forces. It provides pioneering end, effective extensive services in
             Human Resource,
           </p>
-          <p className="ogsfonts18 col-md-11 my-5 aboutusp2 px-5">
+          <p className="ogsfonts18 col-md-11 my-3 aboutusp2 px-5">
             {" "}
             Management services like interviewing and recruitment, training and
             development and also provide the trade testing and other facilities.
@@ -37,6 +41,221 @@ const AboutUs = () => {
             seekers and Employers. For bright future assurance please visit us
             once. - See more at: http://www.ogsmanpower.com
           </p>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-md-11 ogsfonts18 my-5 px-5">
+            <p>OGS (Pvt) Limited. Pakistan</p>
+            <p> Manpower (Recruitment) -Training –HR Services </p>
+            <p>ISO 9001: 2008 Certified</p>
+            <p> Company Profile</p>
+          </div>
+          <div className="col-md-11 ogsfonts18 my-5 px-5">
+            <p>
+              OGS (PVT) Limited is leading group of companies, providing the
+              following services for Manpower Solutions and Human Resources
+              Management.
+            </p>
+            <div className="my-5">
+              <p>
+                OGS Manpower (Contractor) Online interview services provider.
+              </p>
+              <p> Management of Visa Processing.</p>
+              <p> Special Services for Manpower Placement.</p>
+              <p>
+                e-Web Portal for Jobs, Internships, and Training,
+                www.ogsmanpower.com
+              </p>
+              <p>
+                OGS Human Resource Management (Pay roll) /3rd party contractor.
+              </p>
+              <p>OGS Skill Development and Trade Test Center. </p>
+              <p>
+                OGS Consultants General for Oil and Gas Industry OGS
+                Association.
+              </p>
+              <p> OGS Association.</p>
+            </div>
+            <div className="ogsfonts18">
+              <p className="p3ogs">
+                The mission of OGS is to provide Best Quality Manpower. OGS
+                provides recruitment services which are equally successful for
+                job seekers / Candidates and employers / recruiters. Our
+                reputation is one of our most valuable assets. We believe in
+                continued growth and increasing the value of our company by
+                providing the Best Services. A company can only be measured by
+                client satisfaction. It is our mandate to provide the best
+                services in the most effective and efficient manner possible. We
+                do not tolerate unlawful discrimination and committed in
+                providing safe work environment and healthy business.
+              </p>
+            </div>
+            <div className="ogsfonts18 my-4">
+              <p>Vision of OGS:</p>
+              <p className="p3ogs">
+                OGS believes that recruitment begins with a clear statement of
+                objectives, based on the types of knowledge, skills, abilities
+                and other essential characteristics in the recruitment industry.
+                we are committed to consistently provide innovative recruitment
+                solutions by building a strong Professional team focused on
+                market knowledge, relationships and excellence in everything we
+                do. OGS has a vision to become the leader in manpower
+                consultants across the world by building long term positive
+                relationships
+              </p>
+            </div>
+            <div className="ogsfonts18 my-4 p3ogs">
+              <p className="my-5">OGS MANPOWER (AS CONTRACTOR)</p>
+              <p className="p3ogs my-4">
+                OGS Manpower introduces different solutions for the better
+                recruitment in different industries and especially in the sector
+                of
+              </p>
+              <p>Oil and Gas</p>
+              <p>Drilling</p>
+              <p>Construction</p>
+              <p>Bricks and Block,</p>
+              <p>Architecture</p>
+              <p>Furniture and Wood Works</p>
+              <p>Security and Defiance</p>
+              <p>Engineering, Automobile and Transport</p>
+              <p>Air Condition and Refrigeration IT and Telecom</p>
+              <p>Medical, Hospitality</p>
+              <p>Hotel, and Restaurant</p>
+              <p>Insurance</p>
+              <p>Banking and Finance</p>
+              <p>Real Estate and High Voltage</p>
+              <p>Semi Skilled, Skilled and professional Manpower Etc..</p>
+              <p>
+                OGS Manpower has a team of highly qualified, experienced and
+                professional recruitment consultants who know how vital best
+                recruitment is for the success of your business. Whether you
+                need Technical or Managerial Staff, we dedicate our time and
+                required resources to find you the perfect match. Our Human
+                Resource departments are looking out for trust worthy and
+                efficient workers every day. It leads to the rise of recruitment
+                agencies.
+              </p>
+              <p>
+                OGS Manpower Implementing internal quality control and quality
+                assurance (QC/QA) by means of our international Experts.
+              </p>
+              <p>
+                OGS Manpower works to attract the best and brightest manpower
+                pool from Asian countries for various types of overseas jobs. We
+                are dedicated to find out optimum match for both, candidates and
+                our sponsoring Organizations.
+              </p>
+              <p>
+                OGS Manpower Provides International Recruitment Service We use
+                expertise of professional recruitment principles, along with a
+                deep understanding of international manpower recruitment
+                processes. We provide comprehensive HR services and recruitment
+                solutions to maximize our clients' manpower potential. The hunt
+                for quality talent can require overseas recruitment of
+                particular skills. Being a global organization, we have placed
+                candidates across the WORLD.
+              </p>
+              <p>
+                Providing Exclusive Resumes Data Bank; Submission of candidate
+                CVs to the Clients for Screening and checking rigorously the CVs
+                of prospective candidates.
+              </p>
+              <p>
+                Arranging for schedule interviewing; After the candidate has
+                been short listed at our end and is ready to face the final
+                interview, we schedule all the arrangements pertaining to
+                ticketing, traveling and accommodation to ensure that the
+                candidate reaches the place of interview, anywhere in the
+                subcontinent in time.
+              </p>
+              <p>
+                Medical Checkup Complying with the rules and regulation of the
+                country we ensure that the candidate is physically and mentally
+                fit for the job. We get the medical check up done at our end and
+                ask the candidate to undertake the relevant tests. A thorough
+                check up at our end saves significant time and unnecessary
+                complications in foreign land.
+              </p>
+              <p>
+                Orientation program Once the candidate has been selected, before
+                his final departure; we also conduct an orientation program
+                where he is familiarized with different crucial aspects like the
+                work atmosphere of prospective organization, industrial
+                practices, safety issues, labor laws and other relevant issues
+                to avoid any complication.
+              </p>
+              <p>
+                Travel and ticketing arrangements assumes complete
+                responsibility of the candidate, taking care of their traveling
+                documents for the benefit of both, our client as well as the
+                candidate. With our networking and suitable tie ups, we arrange
+                their tickets; provide transport to take them to the airport for
+                departure..
+              </p>
+            </div>
+            <div className="ogsfonts18 my-4 p3ogs">
+              <p>
+                e-WEB PORTAL FOR JOBS, INTERNSHIPS, and TRAINING
+                www.ogsmanpower.com
+              </p>
+              <p>
+                Is a revolutionary new approach in the recruitment? We have a
+                job site that offers both candidates and employers a much
+                improved and much more efficient service. The e-portal contains
+                an advanced system for e-recruitment, which enables job seekers
+                to record their personal data and upload their documents. Our
+                online application system is designed to give prospective
+                employees an excellent opportunity to apply for positions in all
+                fields specially in oil and gas industry. Our extensive database
+                of candidates enables us to conduct an extensive search based
+                upon our Clients’ requirements.
+              </p>
+            </div>
+            <div>
+              <p> Employers:</p>
+              <p className="p3ogs">
+                OGS e-Web portal employers get a great chance of promoting their
+                companies. OGS Manpower provides an employer a user friendly
+                environment to lunge in the manpower pool and select the best
+                candidates for their best companies. Employers get full access
+                to post their jobs for several positions. They are free to
+                search in the Resume Data Bank to some extent and get to know
+                about the employee’s potential. Employers are not just
+                facilitated to post the jobs but also for internship offers. In
+                future OGS Manpower would also facilitate the registered
+                companies to negotiate with other companies for business
+                promotions. Employers can also acquire privilege of getting
+                their employees trained through OGS Manpower Institute.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="px-5">
+          <h1 className="px-5">TEAM</h1>
+          <div className="row justify-content-center my-5">
+            <div className="col-md-3 my-5  d-flex flex-column align-items-center text-center justify-content-center">
+              <img src={teammam} />
+              <h1 className="ogsfonts24">David</h1>
+              <p className="ogsfonts16">ceo</p>
+            </div>
+            <div className="col-md-3 my-5  d-flex flex-column align-items-center text-center justify-content-center">
+              <img src={teammam2} />
+              <h1 className="ogsfonts24">David</h1>
+              <p className="ogsfonts16">ceo</p>
+            </div>
+            <div className="col-md-3  my-5  d-flex flex-column align-items-center text-center justify-content-center">
+              <img src={teammam3} />
+              <h1 className="ogsfonts24">David</h1>
+              <p className="ogsfonts16">ceo</p>
+            </div>
+            <div className="col-md-3 my-5  d-flex flex-column align-items-center text-center justify-content-center">
+              <img src={teammam4} />
+              <h1 className="ogsfonts24">David</h1>
+              <p className="ogsfonts16">ceo</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
