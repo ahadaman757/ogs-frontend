9212cacbaadb84e17e476586dccc421cba932fb8
Kashif Ali Khan <kashifalykhan1999@gmail.com>
2022-10-20 17:54:58 +0500

    ogscourses 5 comit

--- src/Pages/Ogscourses/ogscourses.js
+++ src/Pages/Ogscourses/ogscourses.js
@@ -3,6 +3,41 @@ import "./Ogscourses.css";
 import bulid from "../../Assets/Images/Rectangle 1078.png";
 import bulid2 from "../../Assets/Images/Rectangle 1102.png";
 import btnlogo from "../../Assets/Images/Vector 190.png";
+import Footer from "../../Components/Footer/Footer";
+const data = [
+  {
+    title: "Well Control Course",
+    info: "This course program explains drilling technical terms including well control procedures and technique. The purpose of the core curriculum is to identify a body of knowledge and a set of job skills, which can be used to provide well control skills for drilling operations.",
+    eligibility: "Matric/Fsc/DAE/Graduate",
+    duration: "03 weeks",
+    location: "Rawalpindi",
+    fee: "20,000",
+  },
+  {
+    title: "Well Control Course",
+    info: "This course program explains drilling technical terms including well control procedures and technique. The purpose of the core curriculum is to identify a body of knowledge and a set of job skills, which can be used to provide well control skills for drilling operations.",
+    eligibility: "Matric/Fsc/DAE/Graduate",
+    duration: "03 weeks",
+    location: "Rawalpindi",
+    fee: "20,000",
+  },
+  {
+    title: "Well Control Course",
+    info: "This course program explains drilling technical terms including well control procedures and technique. The purpose of the core curriculum is to identify a body of knowledge and a set of job skills, which can be used to provide well control skills for drilling operations.",
+    eligibility: "Matric/Fsc/DAE/Graduate",
+    duration: "03 weeks",
+    location: "Rawalpindi",
+    fee: "20,000",
+  },
+  {
+    title: "Well Control Course",
+    info: "This course program explains drilling technical terms including well control procedures and technique. The purpose of the core curriculum is to identify a body of knowledge and a set of job skills, which can be used to provide well control skills for drilling operations.",
+    eligibility: "Matric/Fsc/DAE/Graduate",
+    duration: "03 weeks",
+    location: "Rawalpindi",
+    fee: "20,000",
+  },
+];
 
 const Ogscourses = () => {
   return (
@@ -79,12 +114,12 @@ const Ogscourses = () => {
             </p>
           </div>
         </div>
-        <div className="container ">
+        <div className="container py-5">
           <div className="row justify-content-center">
             <div className="col-md-6 bulid2img34">
               {/* <img className=" bulid2img" src={bulid2} /> */}
             </div>
-            <div className="col-md-6">
+            <div className="col-md-6 ">
               <h1 className="ogsfonts32 pt-5 mt-5">
                 OGS Skill Development and Trade Testing
               </h1>
@@ -117,7 +152,44 @@ const Ogscourses = () => {
             </div>
           </div>
         </div>
+        <div className="py-5 ogscardcon">
+          <div className="container my-5">
+            <h1 className="text-center py-5">COURSES</h1>
+            <div className="row justify-content-center">
+              {data.map(
+                ({ title, info, eligibility, duration, location, fee }) => {
+                  return (
+                    <div className="col-md-10 my-4 p-3 cardconoogs">
+                      <div>
+                        <h1 className="ogsfonts24">{title}</h1>
+                        <p className="ogsfonts16">{info}</p>
+                      </div>
+                      <div className="d-flex flex-wrap justify-content-between">
+                        <div>
+                          <p className="m-0 py-1 ogsfonts16">
+                            Eligibility: {eligibility}
+                          </p>
+                          <p className="m-0 py-1 ogsfonts16">
+                            Duration: {duration}
+                          </p>
+                          <p className="m-0 py-1 ogsfonts16">
+                            Location: {location}
+                          </p>
+                        </div>
+                        <div className="d-flex align-items-end justify-content-between">
+                          <p className="ogsfonts16 px-2">Fee: {fee}</p>
+                          <button className="applyogsbtn">Apply Now</button>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                }
+              )}
+            </div>
+          </div>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
