04b4b1fac7c4f1b0dc20b6f9556c5bc3a7472be0
Hazrat Anas <hazratanas@jataq.com>
2022-11-07 22:31:59 +0500

    Main Conflicts Resolved

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./Pages/Homepage/Home";
-import Signin from "./Pages/authpages/Signin";
+import Registeration from "./Pages/authpages/Registeration";
 import AboutUs from "./Pages/about us/Aboutus";
 import Ogscourses from "./Pages/Ogscourses/ogscourses";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
@@ -8,7 +8,7 @@ import Managejobs from "./Pages/ManageJobs/Managejobs";
 import Dashboard from "./Pages/Dashboard/Dashboard";
 import Postajob from "./Pages/post a job/postAjob";
 
-import Registeration from "./Pages/authpages/Registeration";
+
 import CV from "./Pages/Forms/CV";
 
 import SeekerRegisteration from "./Pages/authpages/SeekerRegsiteration";
@@ -28,10 +28,6 @@ const router = createBrowserRouter([
   {
     path: "seeker",
     element: <SeekerRegisteration />,
-  },
-  {
-    path: "signin",
-    element: <Signin />,
   },
   {
     path: "aboutus",
