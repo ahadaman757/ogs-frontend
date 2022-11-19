714ef8c20c3076a8f14097e6ab1f71d7c75f778c
Kashif Ali Khan <kashifalykhan1999@gmail.com>
2022-11-19 16:26:09 +0500

    login edit

---
+++ src/Pages/logins/eplogin.js
@@ -0,0 +1,40 @@
+import Styles from "./login.module.css";
+import bggside from "../../Assets/Images/sign-in-right.png";
+import InputField from "../../Components/inputfield/inputfield";
+const EmployerLogin = () => {
+  return (
+    <div
+      className={`${Styles.auth_page} py-5   container-fluid px-5 primary-bg`}
+    >
+      <div className="container">
+        <div className={`  row `}>
+          <div className={`col-md-6 p-4 ${Styles.auth_page__model}`}>
+            <div className="container-fluid">
+              <h1>Logo</h1>
+              <h3 className={`${Styles.form_heading_1}`}>Employer Login</h3>
+              <p className={`${Styles.form_description}`}>
+                Registration with OGS (Pvt) Ltd is 100% free <br />
+                Please fill up this form to register free at OGS
+              </p>
+              <div className={`pt-5 ${Styles.from}`}>
+                <InputField title={"Email"} />
+                <InputField title={"password"} />
+                <button
+                  className="unset_button w-100 text-white py-2 form_action_button  submit"
+                  type="submit"
+                >
+                  Continue
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className={`col-md-6 p-0  ${Styles.auth_img} `}>
+            <img src={bggside} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default EmployerLogin;
