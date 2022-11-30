cf7f6113ed932ad8e6d52e95b45c98584164ab0f
Kashif Ali Khan <kashifalykhan1999@gmail.com>
2022-11-30 18:53:17 +0500

    updated login

--- src/Pages/logins/eplogin.js
+++ src/Pages/logins/eplogin.js
@@ -7,9 +7,9 @@ const EmployerLogin = () => {
       className={`${Styles.auth_page} py-5   container-fluid px-5 primary-bg`}
     >
       <div className="container">
-        <div className={`  row `}>
+        <div className={`  row d-flex justify-content-center `}>
           <div className={`col-md-6 p-4 ${Styles.auth_page__model}`}>
-            <div className="container-fluid">
+            <div className="">
               <h1>Logo</h1>
               <h3 className={`${Styles.form_heading_1}`}>Employer Login</h3>
               <p className={`${Styles.form_description}`}>
@@ -29,8 +29,8 @@ const EmployerLogin = () => {
             </div>
           </div>
 
-          <div className={`col-md-6 p-0  ${Styles.auth_img} `}>
-            <img src={bggside} />
+          <div className={`col-md-6  p-0  ${Styles.auth_img} `}>
+            <img style={{ width: "100%", height: "100%" }} src={bggside} />
           </div>
         </div>
       </div>
