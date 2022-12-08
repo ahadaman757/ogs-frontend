9704f612d79b7d77a9853c767cac33eeb528d586
ahadaman757 <108423683+ahadaman757@users.noreply.github.com>
2022-12-08 11:42:14 +0500

    GetCompaniesAPI

--- back_end/controllers/generalController.js
+++ back_end/controllers/generalController.js
@@ -1,24 +1,24 @@
-import CustomErrorHandler from "../services/CustomErrorHandler.js";
-import { JoiValidation } from "../validators/JoiValidation.js";
-import { VALID_MODE } from "../config/index.js";
-import User from "../models/User.js";
-import Extractdata from "../services/extractData.js";
-import errorHandler from "../middlewares/errorHandler.js";
-import multer from "multer";
-import path from "path";
-import Company from "../models/CompanyProfile/Company.js";
-import bcrypt from "bcrypt";
-import { REFRESH_SECRET, JWT_SECRET } from "../config/index.js";
-import RefreshToken from "../models/refreshToken.js";
-import { decryptPassword } from "../services/Main.js";
-import jwt_service from "../services/JwtService.js";
-import sequelize from "../config/db.js";
-import bodyParser from "body-parser";
+import CustomErrorHandler from '../services/CustomErrorHandler.js';
+import { JoiValidation } from '../validators/JoiValidation.js';
+import { VALID_MODE } from '../config/index.js';
+import User from '../models/User.js';
+import Extractdata from '../services/extractData.js';
+import errorHandler from '../middlewares/errorHandler.js';
+import multer from 'multer';
+import path from 'path';
+import Company from '../models/CompanyProfile/Company.js';
+import bcrypt from 'bcrypt';
+import { REFRESH_SECRET, JWT_SECRET } from '../config/index.js';
+import RefreshToken from '../models/refreshToken.js';
+import { decryptPassword } from '../services/Main.js';
+import jwt_service from '../services/JwtService.js';
+import sequelize from '../config/db.js';
+import bodyParser from 'body-parser';
 // const User = require('../models/Users')
 const homePageJobsPK = async (req, res, next) => {
   try {
     const jobs = await sequelize.query(
-      "SELECT * FROM job WHERE job.country_id = 167"
+      'SELECT * FROM job WHERE job.country_id = 167'
     );
     res.json({ response: jobs });
   } catch (error) {
@@ -33,7 +33,7 @@ const getPrivacyPolicy = async (req, res, next) => {
     res.json({ code: 1, content: getData });
   } catch (error) {
     console.log(error);
-    res.json({ code: 0, message: "Error getting privacy policy" });
+    res.json({ code: 0, message: 'Error getting privacy policy' });
   }
 };
 const getAboutUs = async (req, res, next) => {
@@ -44,8 +44,19 @@ const getAboutUs = async (req, res, next) => {
     res.json({ code: 1, content: getData });
   } catch (error) {
     console.log(error);
-    res.json({ code: 0, message: "Error getting About Us" });
+    res.json({ code: 0, message: 'Error getting About Us' });
   }
 };
 
-export { homePageJobsPK, getPrivacyPolicy, getAboutUs };
+const getCompanies = async (req, res, next) => {
+  try {
+    const getCompanies = await sequelize.query(
+      'SELECT company_name, company_logo, business_webpage FROM companies'
+    );
+    res.json({ code: 1, getCompanies });
+  } catch (e) {
+    next(e);
+  }
+};
+
+export { homePageJobsPK, getPrivacyPolicy, getAboutUs, getCompanies };
