9e033b9f8ffb5c789dfbd60ea4984147dfdad1ee
ahadaman757 <108423683+ahadaman757@users.noreply.github.com>
2022-12-29 19:31:30 +0500

    jobController

--- back_end/controllers/jobController.js
+++ back_end/controllers/jobController.js
@@ -235,7 +235,7 @@ const getApplicantsForJobById = async (req, res, next) => {
     AND  ((${marital_status} is null) OR cv.marital_status=${marital_status})
     AND  ((${max_experience} is null) OR cv.max_experience=${max_experience})
 `);
-    console.log('Applications ', applicants_record);
+    console.log('Applications ', job_id);
     const applicants_cv_record = applicants_record;
     res.json(applicants_cv_record);
   } catch (error) {
