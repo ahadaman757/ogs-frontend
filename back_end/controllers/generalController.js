5bbb95a58884a4edff1517063b3895ae18d7629f
ahadaman757 <108423683+ahadaman757@users.noreply.github.com>
2023-01-10 03:11:50 +0500

    Job Description API

--- back_end/controllers/generalController.js
+++ back_end/controllers/generalController.js
@@ -90,7 +90,38 @@ const getCourses = async (req, res, next) => {
     next(err);
   }
 };
-
+const GetJobDetailsById = async (req, res, next) => {
+  console.log('id');
+  console.log(req.user.id);
+  try {
+    // const applpied_count=await sequelize.query(`SELECT COUNT(*) FROM job_applicants_cv WHERE job_id=${REQ.}`)
+    const [company_jobs_record, meta] =
+      await sequelize.query(`select j.id,j.job_title, countries.name as country,cities.name as city,careerlevels.career_title, minSalary.max_salary as min_salary,maxSalary.max_salary as max_salary,business_types.business_type_name as industry,
+    genders.gender_title,jobshifts.job_shift,educationqualifications.qualification,j.degree_title,maxAge.max_age as max_age,minAge.max_age as min_age,maxExperience.max_experience as max_experience,minExperience.max_experience as max_experience,DATE(j.valid_upto) as last_date_apply,j.experience_info ,j.job_description, DATE(j.createdAt) AS posted_at,jobtypes.job_type_title
+    from job j 
+   left outer JOIN job_applicants_cv on j.id =job_applicants_cv.job_id
+   left outer JOIN countries on j.country_id=countries.id
+   left outer JOIN  cities on j.city_id=cities.id
+   left outer JOIN careerlevels on career_level_id=careerlevels.id
+   left outer JOIN  maxsalaries minSalary on	min_salary_id =minSalary.id
+   left outer JOIN  maxsalaries maxSalary on	max_salary_id =maxSalary.id
+   left outer JOIN business_types on functional_area_id = business_types.id
+   left outer JOIN genders on gender_title_id  = genders.id
+   left outer JOIN jobshifts on j.job_shift_id  = jobshifts.id
+   left outer JOIN jobtypes on j.job_type_id  = jobtypes.id
+   left outer JOIN educationqualifications on j.required_qualification_id=educationqualifications.id
+   left outer JOIN maxagerequirements maxAge on j.max_age_id=maxAge.id
+   left outer JOIN maxagerequirements minAge on j.min_age_id=minAge.id
+   left outer JOIN maxexperiences maxExperience on j.max_experience_id=maxExperience.id
+   left outer JOIN maxexperiences minExperience on j.min_experience_id=minExperience.id
+    where id = ${req.body.id}
+    `);
+    console.log(meta);
+    res.json(company_jobs_record);
+  } catch (error) {
+    next(error);
+  }
+};
 export {
   homePageJobsPK,
   getPrivacyPolicy,
@@ -98,4 +129,5 @@ export {
   getCompanies,
   homePageJobsIN,
   getCourses,
+  GetJobDetailsById,
 };
