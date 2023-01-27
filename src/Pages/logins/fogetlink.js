534d59b866d59289d13e02c616f151a75a66780c
ahadaman757 <108423683+ahadaman757@users.noreply.github.com>
2023-01-27 18:41:06 +0500

    Website QA #1

--- src/Pages/logins/fogetlink.js
+++ src/Pages/logins/fogetlink.js
@@ -1,41 +1,41 @@
-import Styles from "./login.module.css";
-import logo from "../../Assets/Images/mobilelogo.jpg";
-import { useState } from "react";
-import axios from "axios";
-import { useParams } from "react-router-dom";
-import Redalert from "../../Components/redalert/redalert";
-import Greenalert from "../../Components/greenalert/greenalert";
-import eye from "../../Assets/Images/eye.svg";
-import eyedes from "../../Assets/Images/eye-disable.svg";
+import Styles from './login.module.css';
+import logo from '../../Assets/Images/mobilelogo.jpg';
+import { useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import Redalert from '../../Components/redalert/redalert';
+import Greenalert from '../../Components/greenalert/greenalert';
+import eye from '../../Assets/Images/eye.svg';
+import eyedes from '../../Assets/Images/eye-disable.svg';
 const Frogetlink = () => {
-  const [userPassword, setuserPassword] = useState("");
-  const [userrePassword, setuserrePassword] = useState("");
+  const [userPassword, setuserPassword] = useState('');
+  const [userrePassword, setuserrePassword] = useState('');
   const [erroralert, Seterroralert] = useState(null);
   const { id } = useParams();
   const [logov, setlogov] = useState(eye);
   const [logov2, setlogov2] = useState(eye);
-  const [passwordType, setPasswordType] = useState("password");
-  const [passwordType2, setPasswordType2] = useState("password");
+  const [passwordType, setPasswordType] = useState('password');
+  const [passwordType2, setPasswordType2] = useState('password');
 
   const togglePassword = () => {
-    if (passwordType === "password") {
-      setPasswordType("text");
+    if (passwordType === 'password') {
+      setPasswordType('text');
       setlogov(eyedes);
       return;
     }
-    setPasswordType("password");
+    setPasswordType('password');
     setlogov(eye);
   };
   const togglePassword2 = () => {
-    if (passwordType2 === "password") {
-      setPasswordType2("text");
+    if (passwordType2 === 'password') {
+      setPasswordType2('text');
       setlogov2(eyedes);
       return;
     }
-    setPasswordType2("password");
+    setPasswordType2('password');
     setlogov2(eye);
   };
-  console.log(id, "id");
+  console.log(id, 'id');
   return (
     <>
       <div className={`${Styles.Frogetpassmain}`}>
@@ -52,7 +52,7 @@ const Frogetlink = () => {
                 Fill in your email address to reset your password
               </p>
               <div className="d-flex df">
-                {" "}
+                {' '}
                 <div className="d-flex justify-content-between">
                   <p className="ogsfonts16">Password</p>
                   <p className={`ogsfonts16 ${Styles.InputFieldRe}`}></p>
@@ -125,7 +125,7 @@ const Frogetlink = () => {
                         .then((response) => {
                           if (
                             response.data.message ===
-                            "No account found by email as"
+                            'No account found by email as'
                           ) {
                             Seterroralert(false);
                           } else {
@@ -143,11 +143,11 @@ const Frogetlink = () => {
               </div>
               <div className="mt-2">
                 {erroralert == null ? (
-                  ""
+                  ''
                 ) : erroralert ? (
-                  <Redalert message={"Password dose not match"} />
+                  <Greenalert message={'Please login again'} />
                 ) : (
-                  <Greenalert message={"Please login again"} />
+                  <Redalert message={'Password does not match'} />
                 )}
               </div>
             </div>
