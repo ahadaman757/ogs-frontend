6cdc166c54b30fd1f5eb9c6d5a69744a53874c16
ahadaman757 <108423683+ahadaman757@users.noreply.github.com>
2023-01-29 21:24:09 +0500

    Minor Changes

--- src/Pages/FirstCv/FirstCv.js
+++ src/Pages/FirstCv/FirstCv.js
@@ -1,55 +1,55 @@
-import Styles from "./postajob.module.css";
-import { useState, useEffect } from "react";
-import DashboardNavbar from "../../Components/DashboardNavbar/DashboardNavbar";
-import { TextInput, List, FileUpload, PassInput } from "../Forms/InputFields";
-import TagInput from "../Forms/TagInput";
-import { useFormik } from "formik";
-import * as Yup from "yup";
-import axios from "axios";
-import { UploadImageSide } from "../authpages/Registeration";
-import PhoneInput from "react-phone-number-input";
-import eye from "../../Assets/Images/eye.svg";
-import eyedes from "../../Assets/Images/eye-disable.svg";
+import Styles from './postajob.module.css';
+import { useState, useEffect } from 'react';
+import DashboardNavbar from '../../Components/DashboardNavbar/DashboardNavbar';
+import { TextInput, List, FileUpload, PassInput } from '../Forms/InputFields';
+import TagInput from '../Forms/TagInput';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+import { UploadImageSide } from '../authpages/Registeration';
+import PhoneInput from 'react-phone-number-input';
+import eye from '../../Assets/Images/eye.svg';
+import eyedes from '../../Assets/Images/eye-disable.svg';
 const SignUpCv = () => {
-  const [cvResponse, setcvResponse] = useState("");
+  const [cvResponse, setcvResponse] = useState('');
   const [FormikError, setFormikError] = useState(null);
   const [ProfileImage, setProfileImage] = useState(null);
-  const [cvError, setcvError] = useState("");
+  const [cvError, setcvError] = useState('');
   const [JobTitles, setJobTitles] = useState([]);
-  const [data, Setdata] = useState("");
+  const [data, Setdata] = useState('');
   const [skills, setSkills] = useState();
-  const [Description, setDescription] = useState("");
-  const [dropDownOptions, setdropDownOptions] = useState("");
+  const [Description, setDescription] = useState('');
+  const [dropDownOptions, setdropDownOptions] = useState('');
   const [logov, setlogov] = useState(eye);
   const [logov2, setlogov2] = useState(eye);
-  const [passwordType, setPasswordType] = useState("password");
-  const [passwordType2, setPasswordType2] = useState("password");
+  const [passwordType, setPasswordType] = useState('password');
+  const [passwordType2, setPasswordType2] = useState('password');
   const togglePassword = () => {
-    if (passwordType === "password") {
-      setPasswordType("text");
+    if (passwordType === 'password') {
+      setPasswordType('text');
       setlogov(eyedes);
       return;
     }
-    setPasswordType("password");
+    setPasswordType('password');
     setlogov(eye);
   };
   const togglePassword2 = () => {
-    if (passwordType2 === "password") {
-      setPasswordType2("text");
+    if (passwordType2 === 'password') {
+      setPasswordType2('text');
       setlogov2(eyedes);
       return;
     }
-    setPasswordType2("password");
+    setPasswordType2('password');
     setlogov2(eye);
   };
   console.log(ProfileImage);
   const display = (d) => {
-    console.log("value");
+    console.log('value');
     console.log(d);
     Setdata(d);
   };
   const getjoboptions = () => {
-    axios.get("https://3.14.27.53:3003/jobs/jobsoptions").then((res) => {
+    axios.get('https://3.14.27.53:3003/jobs/jobsoptions').then((res) => {
       setdropDownOptions(res.data);
     });
   };
@@ -59,90 +59,90 @@ const SignUpCv = () => {
   console.log(dropDownOptions);
   const CvFormIk = useFormik({
     initialValues: {
-      passport_photo: "",
-      image: "",
-      first_name: "",
-      last_name: "",
-      email: "",
-      password: "",
-      re_type_password: "",
+      passport_photo: '',
+      image: '',
+      first_name: '',
+      last_name: '',
+      email: '',
+      password: '',
+      re_type_password: '',
       // cv Info
-      interested_in: "",
-      industry: "",
-      job_title: "",
-      f_name: "",
-      gender: "",
-      dob: "",
-      domicile: "",
-      postal_code: "",
-      mobile_number: "",
-      work_number: "",
-      home_number: "",
-      address: "",
-      country: "",
-      city: "",
-      id_card_no: "",
-      passport_number: "",
-      valid_upto: "",
-      passport_issue_date: "",
-      education_level: "",
-      degree_title: "",
-      institution: "",
-      max_experience: "",
-      career_level: "",
-      nationality: "",
-      religion: "",
-      marital_status: "",
-      current_salary: "",
-      expected_salary: "",
-      skin_color: "",
-      weight: "",
-      height: "",
-      min_experience: "",
+      interested_in: '',
+      industry: '',
+      job_title: '',
+      f_name: '',
+      gender: '',
+      dob: '',
+      domicile: '',
+      postal_code: '',
+      mobile_number: '',
+      work_number: '',
+      home_number: '',
+      address: '',
+      country: '',
+      city: '',
+      id_card_no: '',
+      passport_number: '',
+      valid_upto: '',
+      passport_issue_date: '',
+      education_level: '',
+      degree_title: '',
+      institution: '',
+      max_experience: '',
+      career_level: '',
+      nationality: '',
+      religion: '',
+      marital_status: '',
+      current_salary: '',
+      expected_salary: '',
+      skin_color: '',
+      weight: '',
+      height: '',
+      min_experience: '',
     },
     validationSchema: Yup.object({
-      passport_photo: Yup.mixed().required("You need to provide a file"),
-      image: Yup.mixed().required("You need to provide a file"),
+      passport_photo: Yup.mixed().required('You need to provide a file'),
+      image: Yup.mixed().required('You need to provide a file'),
 
-      first_name: Yup.string("invalid type").required("First Name is Required"),
-      last_name: Yup.string("invalid type").required("Required"),
-      email: Yup.string("invalid type").required("Required"),
-      password: Yup.string().required("Password is Required"),
+      first_name: Yup.string('invalid type').required('First Name is Required'),
+      last_name: Yup.string('invalid type').required('Required'),
+      email: Yup.string('invalid type').required('Required'),
+      password: Yup.string().required('Password is Required'),
       re_type_password: Yup.string().oneOf(
-        [Yup.ref("password"), null],
-        "Passwords must match"
+        [Yup.ref('password'), null],
+        'Passwords must match'
       ),
-      interested_in: Yup.string("invalid type").required("Required"),
-      industry: Yup.number("invalid type").required("Required"),
-      job_title: Yup.number("invalid type").required("Required"),
-      f_name: Yup.string("invalid type").required("Required"),
-      gender: Yup.string("invalid type").required("Required"),
-      dob: Yup.string("invalid type").required("Required"),
-      domicile: Yup.string("invalid type").required("Required"),
-      postal_code: Yup.string("invalid type").required("Required"),
-      mobile_number: Yup.string("invalid type").required("Required"),
-      work_number: Yup.string("invalid type").required("Required"),
-      home_number: Yup.string("invalid type").required("Required"),
-      address: Yup.string("invalid type").required("Required"),
-      country: Yup.string("invalid type").required("Required"),
-      city: Yup.string("invalid type").required("Required"),
-      id_card_no: Yup.string("invalid type").required("Required"),
-      passport_number: Yup.string("invalid type").required("Required"),
-      valid_upto: Yup.string("invalid type").required("Required"),
-      education_level: Yup.string("invalid type").required("Required"),
-      degree_title: Yup.string("invalid type").required("Required"),
-      institution: Yup.string("invalid type").required("Required"),
-      max_experience: Yup.string("invalid type").required("Required"),
-      career_level: Yup.string("invalid type").required("Required"),
-      nationality: Yup.string("invalid type").required("Required"),
-      religion: Yup.string("invalid type").required("Required"),
-      marital_status: Yup.string("invalid type").required("Required"),
-      current_salary: Yup.string("invalid type").required("Required"),
-      expected_salary: Yup.string("invalid type").required("Required"),
-      skin_color: Yup.string("invalid type").required("Required"),
-      weight: Yup.string("invalid type").required("Required"),
-      height: Yup.string("invalid type").required("Required"),
-      min_experience: Yup.string("invalid type").required("Required"),
+      interested_in: Yup.string('invalid type').required('Required'),
+      industry: Yup.number('invalid type').required('Required'),
+      job_title: Yup.string('invalid type').required('Required'),
+      f_name: Yup.string('invalid type').required('Required'),
+      gender: Yup.string('invalid type').required('Required'),
+      dob: Yup.string('invalid type').required('Required'),
+      domicile: Yup.string('invalid type').required('Required'),
+      postal_code: Yup.string('invalid type').required('Required'),
+      mobile_number: Yup.string('invalid type').required('Required'),
+      work_number: Yup.string('invalid type').required('Required'),
+      home_number: Yup.string('invalid type').required('Required'),
+      address: Yup.string('invalid type').required('Required'),
+      country: Yup.string('invalid type').required('Required'),
+      city: Yup.string('invalid type').required('Required'),
+      id_card_no: Yup.string('invalid type').required('Required'),
+      passport_number: Yup.string('invalid type').required('Required'),
+      valid_upto: Yup.string('invalid type').required('Required'),
+      education_level: Yup.string('invalid type').required('Required'),
+      degree_title: Yup.string('invalid type').required('Required'),
+      institution: Yup.string('invalid type').required('Required'),
+      max_experience: Yup.string('invalid type').required('Required'),
+      career_level: Yup.string('invalid type').required('Required'),
+      nationality: Yup.string('invalid type').required('Required'),
+      religion: Yup.string('invalid type').required('Required'),
+      marital_status: Yup.string('invalid type').required('Required'),
+      current_salary: Yup.string('invalid type').required('Required'),
+      expected_salary: Yup.string('invalid type').required('Required'),
+      skin_color: Yup.string('invalid type').required('Required'),
+      weight: Yup.string('invalid type').required('Required'),
+      height: Yup.string('invalid type').required('Required'),
+      min_experience: Yup.string('invalid type').required('Required'),
     }),
     onSubmit: (values) => {
       const fullFormData = { ...values };
@@ -151,14 +151,14 @@ const SignUpCv = () => {
         formdata.append(key, fullFormData[key]);
       }
       axios
-        .post("https://3.14.27.53:3003/users", formdata, {
+        .post('https://3.14.27.53:3003/users', formdata, {
           headers: {
-            "Content-Type": "multipart/form-data",
-            "Access-Control-Allow-Origin": "*",
+            'Content-Type': 'multipart/form-data',
+            'Access-Control-Allow-Origin': '*',
           },
         })
         .then((res) => {
-          setcvResponse("Account Created");
+          setcvResponse('Account Created');
         })
         .catch((error) => {
           console.log(error);
@@ -167,8 +167,8 @@ const SignUpCv = () => {
         })
         .finally(() => {
           setTimeout(() => {
-            setcvResponse("");
-            setcvError("");
+            setcvResponse('');
+            setcvError('');
           }, 5000);
         });
     },
@@ -176,7 +176,7 @@ const SignUpCv = () => {
   const [cities, setcities] = useState([]);
   useEffect(() => {
     axios
-      .post("https://3.14.27.53:3003/get_city_by_country_id", {
+      .post('https://3.14.27.53:3003/get_city_by_country_id', {
         country_id: CvFormIk.values.country || 1,
       })
       .then((res) => {
@@ -189,7 +189,7 @@ const SignUpCv = () => {
   }, [CvFormIk.values.country]);
   useEffect(() => {
     axios
-      .post("https://3.14.27.53:3003/get_job_titles_by_industry_id", {
+      .post('https://3.14.27.53:3003/get_job_titles_by_industry_id', {
         industry_id: CvFormIk.values.industry || 1,
       })
       .then((res) => {
@@ -275,9 +275,8 @@ const SignUpCv = () => {
                   />
                 </div>
                 <div className="col-md-6">
-                  <List
+                  <TextInput
                     id="job_title"
-                    options={JobTitles}
                     label="Job Title"
                     formik={CvFormIk}
                   />
@@ -547,7 +546,7 @@ const SignUpCv = () => {
               <hr />
               <div className="d-flex justify-content-end">
                 <button type="submit" className={`mx-2 ${Styles.btnPost}`}>
-                  Register{" "}
+                  Register{' '}
                 </button>
               </div>
             </div>
