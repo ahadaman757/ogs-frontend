602643094fe77e1c9e1a5ae9d9bddb7517d6eea0
ahadaman757 <108423683+ahadaman757@users.noreply.github.com>
2023-01-29 23:45:46 +0500

    Minor Changes

--- src/Pages/authpages/Institute.js
+++ src/Pages/authpages/Institute.js
@@ -1,20 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-import styles from "./main.module.css";
-import logo from "../../Assets/Images/mobilelogo.jpg";
-import MultiStep from "react-multistep";
-import eye from "../../Assets/Images/eye.svg";
-import eyedes from "../../Assets/Images/eye-disable.svg";
+import styles from './main.module.css';
+import MultiStep from 'react-multistep';
+import eye from '../../Assets/Images/eye.svg';
+import eyedes from '../../Assets/Images/eye-disable.svg';
 
 import {
   LoginInformationValidation,
   BusinessInformationValidation,
   AddressInformationValidation,
-} from "../../formsValidations/Registeration";
-import { List, TextInput, PassInput } from "../Forms/InputFields";
-import { useFormik } from "formik";
-import axios from "axios";
-import mobilelogo from "../../Assets/Images/mobilelogo.jpg";
+} from '../../formsValidations/Registeration';
+import {
+  List,
+  TextInput,
+  PassInput,
+  SecondTextInput,
+} from '../Forms/InputFields';
+import { useFormik } from 'formik';
+import axios from 'axios';
+import mobilelogo from '../../Assets/Images/mobilelogo.jpg';
 
 const UploadImageSide = ({ setLogoData, title, formik, id }) => {
   const [photoSelected, setphotoSelected] = useState();
@@ -35,7 +39,7 @@ const UploadImageSide = ({ setLogoData, title, formik, id }) => {
             {photoSelected == null ? (
               <img
                 className={`img-fluid ${styles.file_upload_icon} `}
-                src={require("../../Assets/Images/file upload.png")}
+                src={require('../../Assets/Images/file upload.png')}
                 alt=""
               />
             ) : (
@@ -67,30 +71,46 @@ const LoginInformation = ({
   formData,
   employerRegsiterOptions,
 }) => {
+  function generateCode(length, characters) {
+    var result = '';
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+    return result;
+  }
+
   const [positions, setpostions] = useState();
   const [loginInfo, setloginInfo] = useState(null);
+  const [isVerified, setIsVerified] = useState(false);
+  const [codeSent, setCodeSent] = useState(false);
+  const [codeGenerated, setCodeGenerated] = useState();
   const [logov, setlogov] = useState(eye);
   const [logov2, setlogov2] = useState(eye);
-  const [passwordType, setPasswordType] = useState("password");
-  const [passwordType2, setPasswordType2] = useState("password");
+  const [passwordType, setPasswordType] = useState('password');
+  const [passwordType2, setPasswordType2] = useState('password');
+  const [message, setMessage] = useState();
+  const [code, setCode] = useState();
   const togglePassword = () => {
-    if (passwordType === "password") {
-      setPasswordType("text");
+    if (passwordType === 'password') {
+      setPasswordType('text');
       setlogov(eyedes);
       return;
     }
-    setPasswordType("password");
+    setPasswordType('password');
     setlogov(eye);
   };
   const togglePassword2 = () => {
-    if (passwordType2 === "password") {
-      setPasswordType2("text");
+    if (passwordType2 === 'password') {
+      setPasswordType2('text');
       setlogov2(eyedes);
       return;
     }
-    setPasswordType2("password");
+    setPasswordType2('password');
     setlogov2(eye);
   };
+
   useEffect(() => {
     setpostions(employerRegsiterOptions?.positions);
   }, [employerRegsiterOptions]);
@@ -147,34 +167,116 @@ const LoginInformation = ({
               label="Email"
             />
           </div>
+          {message}
+          <br />
+          <div className="col-12">
+            <div className="row">
+              <div className="col-md-6">
+                <input
+                  placeholder="Enter Verification Code"
+                  style={{
+                    padding: '6px 9px',
+                    width: '100%',
+                    border: '1px solid lightgray',
+                  }}
+                  onChange={(e) => setCode(e.target.value)}
+                />
+              </div>
+              <div className="col-md-6">
+                {codeSent ? (
+                  <span
+                    type="button"
+                    className={`unset_button w-100 text-white py-2 form_action_button  submit ${styles.sobtn}`}
+                    style={{ textAlign: 'center' }}
+                    onClick={() => {
+                      if (code == codeGenerated) {
+                        setIsVerified(true);
+                        setMessage('You have verified your email.');
+                      } else {
+                        setMessage('Incorrect Code');
+                      }
+                    }}
+                  >
+                    Verify
+                  </span>
+                ) : (
+                  <span
+                    className={`unset_button w-100 text-white py-2 form_action_button  submit ${styles.sobtn}`}
+                    type="button"
+                    style={{ textAlign: 'center' }}
+                    onClick={() => {
+                      setTimeout(() => {
+                        localStorage.setItem(
+                          'uMail',
+                          logininformationFormik.values.email
+                        );
+                        let generatedCode = generateCode(6, '1234567890');
+                        setCodeGenerated(generatedCode);
+                        axios
+                          .post(`https://3.14.27.53:3003/general/verifyEmail`, {
+                            userEmail: localStorage.getItem('uMail'),
+                            token: generatedCode,
+                          })
+                          .then((response) => {
+                            console.log(response);
+                            if (response.data.code == 1) {
+                              setMessage('Please check your email');
+                              setCodeSent(true);
+                            } else {
+                              setMessage(response.data.message);
+                            }
+                          });
+                      }, 2000);
+                    }}
+                  >
+                    Get Code
+                  </span>
+                )}
+              </div>
+            </div>
+          </div>
           <div className="col-md-6">
             <PassInput
               type="password"
               id="password"
               formik={logininformationFormik}
               label="Password"
-              togglePassword={togglePassword2}
-              logov={logov2}
-              passwordType={passwordType2}
-            />
-          </div>
-          <div className="col-md-6">
-            <PassInput
-              type="password"
-              id="repeat_password"
-              formik={logininformationFormik}
-              label="Repeat Password"
               togglePassword={togglePassword}
               logov={logov}
               passwordType={passwordType}
             />
           </div>
-          <button
-            className={`unset_button w-100 text-white py-2 form_action_button  submit ${styles.sobtn}`}
-            type="submit"
-          >
-            Continue
-          </button>
+          <div className="col-md-6">
+            {/* <PassInput
+              id="repeat_password"
+              formik={logininformationFormik}
+              label="Repeat Password"
+            /> */}
+            <PassInput
+              type="password"
+              id="repeat_password"
+              formik={logininformationFormik}
+              label="Repeat Password"
+              togglePassword={togglePassword2}
+              logov={logov2}
+              passwordType={passwordType2}
+            />
+          </div>
+          {isVerified ? (
+            <button
+              className={`unset_button w-100 text-white py-2 form_action_button  submit ${styles.sobtn}`}
+              type="submit"
+            >
+              Continue
+            </button>
+          ) : (
+            <span
+              className={`unset_button w-100 text-white py-2 form_action_button  submit ${styles.sobtn}`}
+              style={{ textAlign: 'center' }}
+            >
+              Verify Email To Continue
+            </span>
+          )}
         </form>
       </div>
     </>
@@ -234,6 +336,11 @@ const Businessinformation = ({
               formik={BusinessinformationFormik}
               label="Mobile Number"
             />
+            <UploadImageSide
+              id="image"
+              formik={BusinessinformationFormik}
+              title="Logo Upload"
+            />
           </div>
           <button
             className={`unset_button w-100 text-white py-2 form_action_button  submit ${styles.sobtn}`}
@@ -276,11 +383,10 @@ const AddressDetails = ({
   }, []);
   useEffect(() => {
     axios
-      .post("https://3.14.27.53:3003/get_city_by_country_id", {
+      .post('https://3.14.27.53:3003/get_city_by_country_id', {
         country_id: AddressinformationFormik.values.country || 1,
       })
       .then((res) => {
-        console.log(res);
         setcities(res.data);
       })
       .catch((error) => {
@@ -365,7 +471,7 @@ function Instituteregister() {
   const [employerRegsiterOptions, setemployerRegsiterOptions] = useState();
   useEffect(() => {
     axios
-      .get("https://3.14.27.53:3003/employer_register_options")
+      .get('https://3.14.27.53:3003/employer_register_options')
       .then((res) => {
         setemployerRegsiterOptions(res.data);
       })
@@ -375,21 +481,22 @@ function Instituteregister() {
   }, []);
   const [LogoData, setLogoData] = useState();
   const [formData, setformData] = useState({
-    position: "",
-    first_name: "",
-    last_name: "",
-    email: "",
-    password: "",
-    repeat_password: "",
-    businessName: "",
-    businessType: "",
-    businessWebpage: "",
-    mobileNumber: "",
-    address: "",
-    country: "",
-    employerName: "",
-    employerNumber: "",
-    employerEmail: "",
+    position: '',
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: '',
+    repeat_password: '',
+    businessName: '',
+    businessType: '',
+    businessWebpage: '',
+    image: '',
+    mobileNumber: '',
+    address: '',
+    country: '',
+    employerName: '',
+    employerNumber: '',
+    employerEmail: '',
   });
   const [formStep, setformStep] = useState(1);
 
@@ -398,7 +505,7 @@ function Instituteregister() {
   };
   const steps = [
     {
-      name: "StepOne",
+      name: 'StepOne',
       component: (
         <div>
           <LoginInformation
@@ -411,7 +518,7 @@ function Instituteregister() {
       ),
     },
     {
-      name: "StepTwo",
+      name: 'StepTwo',
       component: (
         <div>
           <Businessinformation
@@ -424,7 +531,7 @@ function Instituteregister() {
       ),
     },
     {
-      name: "StepThree",
+      name: 'StepThree',
       component: (
         <div>
           <AddressDetails
@@ -447,17 +554,17 @@ function Instituteregister() {
         <div className={` ${styles.auth_page__model} row `}>
           <div
             className={`${
-              formStep == 4 ? " col-md-12" : "col-md-6"
+              formStep == 4 ? ' col-md-12' : 'col-md-6'
             }  d-flex justify-content-center align-items-center py-md-4 py-2 px-md-5 px-2`}
-            style={{ overflow: "hidden" }}
+            style={{ overflow: 'hidden' }}
           >
             <div className="container-fluid">
               <div className="d-flex justify-content-center">
-                <img src={mobilelogo} />{" "}
+                <img src={mobilelogo} />{' '}
               </div>
               <div className="text-center my-4">
                 <h3 className={`${styles.form_heading_1}`}>
-                  Institute Registeration
+                  Agent Registeration
                 </h3>
                 <p className={`${styles.form_description}`}>
                   Registration with OGS Man Power is 100% free <br />
@@ -470,11 +577,11 @@ function Instituteregister() {
                   <>
                     <div
                       className={` form_step ${
-                        formStep != 4 ? "slide_button" : "disable_slide_button"
+                        formStep != 4 ? 'slide_button' : 'disable_slide_button'
                       } `}
                     >
                       <MultiStep
-                        prevStyle={{ backgroundColor: "red" }}
+                        prevStyle={{ backgroundColor: 'red' }}
                         activeStep={0}
                         showNavigation={true}
                         steps={steps}
@@ -484,7 +591,7 @@ function Instituteregister() {
                       <button
                         className={`primary-bg form_action_button text-white unset_button ogsfonts15 py-2 $`}
                       >
-                        {" "}
+                        {' '}
                         Register
                       </button>
                     ) : null}
