746c1bdb7abf93e39275e50284fe44799c329ef6
ahadaman757 <108423683+ahadaman757@users.noreply.github.com>
2023-02-06 13:43:11 +0500

    Update newapplicant.js

--- src/Pages/new applicant/newapplicant.js
+++ src/Pages/new applicant/newapplicant.js
@@ -212,7 +212,7 @@ const Newapplicant = () => {
                     <span>
                       <img
                         src="https://cdn-icons-png.flaticon.com/512/481/481874.png"
-                        onClick={() => navigate(`/admineditjob/${item.id}`)}
+                        onClick={() => navigate(`/admineditjob/${jobdes.id}`)}
                         style={{ width: '13px' }}
                       />
                     </span>
