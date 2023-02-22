066e40350d7b6327e78b27913492613b27c070de
ahadaman757 <108423683+ahadaman757@users.noreply.github.com>
2023-02-22 19:08:06 +0500

    Update CvContoller.js

--- back_end/controllers/CvContoller.js
+++ back_end/controllers/CvContoller.js
@@ -88,11 +88,13 @@ const CreateCv = async (req, res, next) => {
     const insert_cv = await sequelize.query(
       `insert INTO cv (cv_image,job_title,career_level,dob,domicile,postal_code,mobile_number,work_number,home_number,address,country,city,id_card_no,passport_number,passport_photo,valid_upto,passport_issue_date,degree_title,institution,first_name,last_name,max_experience,min_experience,industry,education_level,gender,interested_in,position,nationality,religion,marital_status,current_salary,expected_salary,skin_color,weight,height,user_id) VALUES('${req.files?.image[0]?.path}', '${job_title}', ${career_level}, '${dob}','${domicile}',${postal_code},${mobile_number},${work_number},${home_number},'${address}',${country},${city},'${id_card_no}','${passport_number}','${req.files?.passport_photo[0]?.path}','${valid_upto}','${passport_issue_date}','${degree_title}','${institution}','${first_name}','${last_name}',${max_experience},${min_experience},${industry},${education_level},${gender},${interested_in},${position},${nationality},${religion},${marital_status},${current_salary},${expected_salary},'${skin_color}',${weight}, ${height},${userID})`
     );
-    // const insert_code = await sequelize.query(
-    //     `insert INTO cv (cv_image,job_title,career_level,dob,domicile,postal_code,mobile_number,work_number,home_number,address,country,city,id_card_no,passport_number,passport_photo,valid_upto,passport_issue_date,degree_title,institution,first_name,last_name,max_experience,min_experience,industry,education_level,gender,interested_in,position,nationality,religion,marital_status,current_salary,expected_salary,skin_color,weight,height,user_id) VALUES('${req.files?.image[0]?.path}', '${job_title}', ${career_level}, '${dob}','${domicile}',${postal_code},${mobile_number},${work_number},${home_number},'${address}',${country},${city},'${id_card_no}','${passport_number}','${req.files?.passport_photo[0]?.path}','${valid_upto}','${passport_issue_date}','${degree_title}','${institution}','${first_name}','${last_name}',${max_experience},${min_experience},${industry},${education_level},${gender},${interested_in},${position},${nationality},${religion},${marital_status},${current_salary},${expected_salary},'${skin_color}',${weight}, ${height},${userID})`
-    // );
+
+    let code = makeid(10);
+    const insert_code = await sequelize.query(
+      `INSERT INTO code_generated (cv_id, code) VALUES ('${insert_cv[0]}', '${code}')`
+    );
     console.log(insert_cv);
-    console.log(insert_cv[0]);
+    console.log(insert_code);
     res.json({ message: "cv added" });
   } catch (error) {
     next(error);
